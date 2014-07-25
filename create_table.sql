9851639573f04c6a92af7178ea42046cb676ce7b
Ubuntu <nskelsey@gmail.com>
2014-07-25 15:38:14 +0000

    removing tabs

--- create_table.sql
+++ create_table.sql
@@ -1,18 +1,18 @@
 CREATE TABLE blocks (
-	hash TEXT NOT NULL, 
-	prevhash TEXT, 
+    hash TEXT NOT NULL, 
+    prevhash TEXT, 
     height INT, -- The number of blocks between this one and the genesis block.
-	PRIMARY KEY (hash)
+    PRIMARY KEY (hash)
     FOREIGN KEY(prevhash) REFERENCES blocks(hash)
 );
 
 CREATE TABLE bulletins (
     author TEXT NOT NULL, -- From the address of the first OutPoint used.
-	txid TEXT NOT NULL, 
+    txid TEXT NOT NULL, 
     topic TEXT,   -- UTF-8
     message TEXT, -- UTF-8
     block TEXT, 
-	PRIMARY KEY (txid), 
+    PRIMARY KEY (txid), 
     FOREIGN KEY(block) REFERENCES blocks (hash)
 );
 
