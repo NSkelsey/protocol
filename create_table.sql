c0b27df41ee7f26303d0dd70638a4a52c7feec30
Ubuntu <nskelsey@gmail.com>
2014-07-25 16:38:34 +0000

    more formatting

--- create_table.sql
+++ create_table.sql
@@ -1,17 +1,19 @@
 CREATE TABLE blocks (
-    hash TEXT NOT NULL, 
-    prevhash TEXT, 
-    height INT, -- The number of blocks between this one and the genesis block.
-    PRIMARY KEY (hash)
+    hash        TEXT NOT NULL, 
+    prevhash    TEXT, 
+    height      INT,        -- The number of blocks between this one and the genesis block.
+    -- Table constraints
+    PRIMARY KEY(hash)
     FOREIGN KEY(prevhash) REFERENCES blocks(hash)
 );
 
 CREATE TABLE bulletins (
-    author TEXT NOT NULL, -- From the address of the first OutPoint used.
-    txid TEXT NOT NULL, 
-    topic TEXT,   -- UTF-8
-    message TEXT, -- UTF-8
-    block TEXT, 
+    author  TEXT NOT NULL,  -- From the address of the first OutPoint used.
+    txid    TEXT NOT NULL, 
+    topic   TEXT,           -- UTF-8
+    message TEXT,           -- UTF-8
+    block   TEXT, 
+    -- Table constraints
     PRIMARY KEY (txid), 
     FOREIGN KEY(block) REFERENCES blocks (hash)
 );
