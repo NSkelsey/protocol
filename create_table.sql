5172cb04e58c02d5ae228e00821673ff060c2fad
Ubuntu <nskelsey@gmail.com>
2014-10-12 02:19:52 +0000

    Enforce non-null msg in sqlite

--- create_table.sql
+++ create_table.sql
@@ -1,4 +1,4 @@
--- DB Schema -- Version 0.1.1
+-- DB Schema -- Version 0.1.2
 
 
 CREATE TABLE blocks (
@@ -15,7 +15,7 @@ CREATE TABLE bulletins (
     author      TEXT NOT NULL,  -- From the address of the first OutPoint used.
     txid        TEXT NOT NULL, 
     board       TEXT,           -- UTF-8
-    message     TEXT,           -- UTF-8
+    message     TEXT NOT NULL,  -- UTF-8, must have some content.
     timestamp   INT,            -- Seconds since Jan 1, 1970
     block       TEXT,
 
